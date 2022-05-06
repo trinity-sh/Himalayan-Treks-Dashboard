4f9ff252d7c6871a2bd5c8accee7abd60d9f543c
jay-io <jay.dnb@outlook.in>
2022-05-06 13:56:07 +0530

    Add gradient btn react component

---
+++ src/components/buttonGrad.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/button-grad.css';
+
+function buttonGrad(props) {
+  return (
+    <div id='btn-grad-parent-div'>
+      <button id={props.id} className='btn-grad' onClick={() => props.onClickCallback()}>
+        <span id={props.icon_id} className='material-symbols-outlined btn-grad-icon'>
+          {props.icon}
+        </span>
+        <span id={props.label_id} className='btn-grad-label'>
+          &nbsp;&nbsp;{props.label}
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default buttonGrad;
