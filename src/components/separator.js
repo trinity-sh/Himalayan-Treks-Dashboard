134302699229e5bd3edae242ac71d3468c6aedea
jay-io <jay.dnb@outlook.in>
2022-05-08 13:57:30 +0530

    Add side-panel separator react component

---
+++ src/components/separator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function separator(props) {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row', marginTop: props.marginTop, marginBottom: props.marginBottom, alignItems: 'center' }}>
+      <div style={{ flex: '1' }} />
+      <div style={{ flex: '1', backgroundColor: 'black', height: '1px', position: 'relative', top: '50%' }} />
+      <div style={{ flex: '3', fontFamily: "Montserrat", fontSize: '13px', fontWeight: '700' }}>{props.msg}</div>
+      <div style={{ flex: '1', backgroundColor: 'black', height: '1px' }} />
+      <div style={{ flex: '1' }} />
+    </div>
+  );
+}
+
+export default separator;
