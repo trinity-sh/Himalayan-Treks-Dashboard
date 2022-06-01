33a6377c6a012f81e6052d415eaa7f3920deec14
jay-io <jay.dnb@outlook.in>
2022-06-02 02:11:42 +0530

    Add mobile layout responsible side panel

--- src/components/sidePanel.js
+++ src/components/sidePanel.js
@@ -3,14 +3,16 @@ import ButtonGrad from '../components/buttonGrad';
 import Separator from '../components/separator';
 import SelectableMenu from '../components/selectableMenu';
 import Logo from '../assets/logo.png';
+import '../styles/content-div.css';
 import '../styles/side-panel.css';
-import { useNavigate } from 'react-router-dom';
 
 function sidePanel() {
-  const navigate = useNavigate();
   const [sectBtn, setSectBtnState] = useState(() => {
     return { invoked: 'dash-btn' };
   });
+
+  const [panelIsOpen, setPanelIsOpen] = useState(true);
+  const [secondPanelIsOpen, setSecondPanelIsOpen] = useState(false);
 
   const onClickDeselectOthers = (thisBtn) => {
     var children = document.getElementById('selectables').getElementsByTagName('div');
@@ -40,21 +42,84 @@ function sidePanel() {
     document.getElementById('prof-btn-label').className = 'btn-grad-active-label';
   };
 
+  useEffect(() => {
+    const adjustSidePanelWidth = () => {
+      if (secondPanelIsOpen)
+        setSecondPanelIsOpen(false);
+      if (window.innerWidth < 800) {
+        setPanelIsOpen(false);
+      } else {
+        setPanelIsOpen(true);
+      }
+    };
+    window.addEventListener('resize', adjustSidePanelWidth);
+    // initial rendering after DOM Loads
+    adjustSidePanelWidth();
+  }, []);
+
+  const regulateSidePanel = () => {
+    let sidePanel = document.getElementById('side-panel');
+    let floatingBtn = document.getElementById('floating-btn');
+    if (!panelIsOpen) {
+      floatingBtn.style.visibility = 'visible';
+      sidePanel.style.visibility = 'hidden';
+      sidePanel.style.width = '0%';
+      sidePanel.style.minWidth = '0px';
+    } else {
+      floatingBtn.style.visibility = 'hidden';
+      sidePanel.style.visibility = 'visible';
+      sidePanel.style.width = '15%';
+      sidePanel.style.minWidth = '260px';
+    }
+  };
+
+  const regulateSecondSidePanel = () => {
+    let secondSidePanel = document.getElementById('second-side-panel');
+    if (!secondPanelIsOpen) {
+      secondSidePanel.style.visibility = 'hidden';
+      secondSidePanel.style.width = '0%';
+      secondSidePanel.style.minWidth = '0px';
+    } else {
+      secondSidePanel.style.visibility = 'visible';
+      secondSidePanel.style.width = '15%';
+      secondSidePanel.style.minWidth = '260px';
+    }
+  };
+
+  useEffect(() => regulateSidePanel(), [panelIsOpen]);
+  useEffect(() => regulateSecondSidePanel(), [secondPanelIsOpen]);
+
   // default selection is 'dashboard/all-treks'
-  useEffect(() => { onClickDashBtn(); navigate('/'); }, []);
+  useEffect(() => { onClickDashBtn(); }, []);
 
   return (
-    <div id='side-panel'>
-      <div style={{ marginTop: '50px', marginBottom: '20px' }}>
-        <img src={Logo} width='60%' alt='Himalayan backdrop' />
+    <>
+      <div id='side-panel'>
+        <div style={{ marginTop: '50px', marginBottom: '20px' }}>
+          <img src={Logo} width='60%' alt='Himalayan backdrop' />
+        </div>
+        <div id='selectables'>
+          <ButtonGrad id='dash-btn' icon_id='dash-btn-icon' label_id='dash-btn-label' icon='tune' label='Dashboard' onClickCallback={onClickDashBtn} />
+          {/* <ButtonGrad id='prof-btn' icon_id='prof-btn-icon' label_id='prof-btn-label' icon='badge' label='Profile settings' onClickCallback={onClickProfBtn} /> */}
+        </div>
+        <Separator marginTop='20px' marginBottom='25px' msg='Menu items' />
+        <SelectableMenu invoked={sectBtn.invoked} />
       </div>
-      <div id='selectables'>
-        <ButtonGrad id='dash-btn' icon_id='dash-btn-icon' label_id='dash-btn-label' icon='tune' label='Dashboard' onClickCallback={onClickDashBtn} />
-        <ButtonGrad id='prof-btn' icon_id='prof-btn-icon' label_id='prof-btn-label' icon='badge' label='Profile settings' onClickCallback={onClickProfBtn} />
-      </div>
-      <Separator marginTop='20px' marginBottom='25px' msg='Menu items' />
-      <SelectableMenu invoked={sectBtn.invoked} />
-    </div>
+      <>
+        <button id='floating-btn' className='button-update' onClick={() => setSecondPanelIsOpen(!secondPanelIsOpen)}>{secondPanelIsOpen ? '<' : '>'}</button>
+        <div id='second-side-panel' className='side-panel'>
+          <div style={{ marginTop: '50px', marginBottom: '20px' }}>
+            <img src={Logo} width='60%' alt='Himalayan backdrop' />
+          </div>
+          <div id='selectables'>
+            <ButtonGrad id='dash-btn' icon_id='dash-btn-icon' label_id='dash-btn-label' icon='tune' label='Dashboard' onClickCallback={onClickDashBtn} />
+            {/* <ButtonGrad id='prof-btn' icon_id='prof-btn-icon' label_id='prof-btn-label' icon='badge' label='Profile settings' onClickCallback={onClickProfBtn} /> */}
+          </div>
+          <Separator marginTop='20px' marginBottom='25px' msg='Menu items' />
+          <SelectableMenu invoked={sectBtn.invoked} />
+        </div>
+      </>
+    </>
   );
 }
 
